1ce4b8e7084c3e352f6b2cb2955af8cf64ba176d
asad ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-09 16:46:11 +0500

    Create task26.js
    done

---
+++ task26.js
@@ -0,0 +1,12 @@
+
+function points(alien_color){
+if(alien_color=== "green"){
+    return console.log("the player just earned 5 points for shooting the alien");
+}
+else 
+{
+    return "print a statement that the player just earned 10 points."
+};
+}
+points("green");
+//points("blue");

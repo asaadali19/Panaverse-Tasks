5d32fc68beff3e9976e4ac32caaa5a6f42899027
asad ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-09 09:57:22 +0500

    Create task2.js
    task 2 is done

---
+++ task2.js
@@ -0,0 +1,7 @@
+//Store a person’s name in a variable, and print a message to that person. Your message should be simple, 
+//such as, “Hello Eric, would you like to learn some Python today?”
+
+let person_name = "asad ali";
+
+console.log(person_name + ", would you like to learn some Python today?");
+

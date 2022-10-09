d8c3ed6bf54aaf3601fb63d5d080fc955b2b7fe4
asad ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-09 17:19:20 +0500

    Update task28.js
    removed return

--- task28.js
+++ task28.js
@@ -4,22 +4,22 @@ function personStage(age){
         return console.log("the person is a baby.");
     }
     else if(age<4 && age >=2){
-        return console.log("the person is a toddler.");
+         console.log("the person is a toddler.");
     }
     else if(age >=4 && age <13){
-        return console.log("the person is a kid");
+         console.log("the person is a kid");
     }
     else if(age >=13 && age <20){
-        return console.log("the person is a teenager.");
+         console.log("the person is a teenager.");
     }
     else if(age >=20 && age <65){
-        return console.log("the person is an adult.");
+         console.log("the person is an adult.");
     }
     else{
-        return console.log("the person is an elder.");
+         console.log("the person is an elder.");
     }
 
 
 }
 personStage(20);
-//personStage(1);
+personStage(1);

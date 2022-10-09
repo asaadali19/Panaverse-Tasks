ef0d05e51e17b9a178017b2d3020816d627c655c
asad ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-09 11:10:56 +0500

    Create task7.js
    task 7 is completed

---
+++ task7.js
@@ -0,0 +1,16 @@
+//Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure 
+//to enclose your operations in print statements to see the results.
+
+let a =5;
+let b= 3;
+console.log(a+b);
+a = 16;
+b = 2;
+console.log(a/b);
+a = 16;
+b = 8;
+console.log(a-b);
+a = 4;
+b = 2;
+
+console.log(a*b);

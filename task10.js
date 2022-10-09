888c6b245f0d575fe594672c3ee022f79369ecca
asad ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-09 11:20:37 +0500

    Create task10.js
    Task 10 is completed

---
+++ task10.js
@@ -0,0 +1,23 @@
+//Adding Comments for prog 1:
+
+let person_name = " \tAsad\n";
+console.log(person_name);
+
+console.log(person_name.trim());
+
+// Adding Comments for prog 2:
+// this program is adding multi.
+
+let a =5;
+let b= 3;
+console.log(a+b);
+a = 16;
+b = 2;
+console.log(a/b);
+a = 16;
+b = 8;
+console.log(a-b);
+a = 4;
+b = 2;
+
+console.log(a*b);

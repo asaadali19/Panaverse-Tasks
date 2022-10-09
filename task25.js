08ccb9bf8d410c641b38020610d41c2d088525b1
asad ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-09 17:19:06 +0500

    Update task25.js
    return removed

--- task25.js
+++ task25.js
@@ -1,13 +1,10 @@
     let alien_color = "green";
 
     if(alien_color=== "green"){
-        return console.log("the player just earned 5 points.");
+        console.log("the player just earned 5 points.");
     }
     alien_color = "blue";
-    if(alien_color=== "green"){
-        return console.log("the player just earned 5 points.");
+    if(alien_color!= "green"){
+         console.log("");
     }
-    else 
-    {
-        return false
-    };
+   

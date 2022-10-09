26057c21689cc191941ac6117c68e5426e51418d
asad ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-09 16:59:37 +0500

    Update task26.js
    changes done

--- task26.js
+++ task26.js
@@ -1,16 +1,12 @@
 
 function points(alien_color){
 if(alien_color=== "green"){
-    return console.log("player earned 5 points");
+    return console.log("the player just earned 5 points for shooting the alien");
 }
-else if(alien_color==="yellow")
+else 
 {
-    return "player earned 10 points";
-}
-else{
-    return "player earned 15 points";
-}
+    return "print a statement that the player just earned 10 points."
+};
 }
 points("green");
-//points("blue");
-//points("yellow")
+//points("yellow");

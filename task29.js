33878bbdac84ca340a45549cf478bb4780f7310d
asad ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-09 17:19:26 +0500

    Create task29.js
    done

---
+++ task29.js
@@ -0,0 +1,12 @@
+let favorite_fruits = ["apple", "oranges", "bananas"];
+
+if(favorite_fruits.includes("bananas")){
+     console.log("You really like bananas");
+}
+if(favorite_fruits.includes("oranges")){
+   console.log("You really like oranges");
+}
+if(favorite_fruits.includes("apple")){
+    return console.log("You really like apple");
+}
+

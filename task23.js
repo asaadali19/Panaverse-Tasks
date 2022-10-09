542d9faa143c06a5197770fdf630ec5d1b1f76c6
asad ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-09 15:36:04 +0500

    Create task23.js
    done

---
+++ task23.js
@@ -0,0 +1,24 @@
+
+
+function check(car){
+    return car=='subar' ? true : false;
+
+}
+
+console.log(check("abc"));
+console.log(check("subar"));
+
+function check1(caseSen){
+    return caseSen=='Subar' ? true : false;
+
+}
+
+console.log(check1("abc"));
+console.log(check1("Subar"));
+
+function check2(intgr){
+    return intgr===10 ? true : false;
+
+}
+console.log(check2(10));
+console.log(check2("Subar"));

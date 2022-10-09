5e2fe10780251b3567c3ceb89f40f4391a1918ab
asad ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-09 11:33:03 +0500

    Create task13.js
    done

---
+++ task13.js
@@ -0,0 +1,9 @@
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list 
+// that stores several examples. Use your list to print a series of statements about these items, such as “I would 
+// like to own a Honda motorcycle.”
+
+let ownArray = ["aeroPlane", "Iron man suit", "car"];
+
+for(i=0; i<ownArray.length; i++){
+    console.log("my favourte transportation mode is " + ownArray[i]);
+}

8a1c257c14098baf1c54df591bb9340ff646cc53
asad ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-09 11:23:27 +0500

    Create task11.js
    Task 11 is completed

---
+++ task11.js
@@ -0,0 +1,7 @@
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let nameArray = ["asad", "ali", "saqib", "wasif", "asif"];
+
+for(i=0; i<nameArray.length; i++){
+    console.log(nameArray[i]);
+}

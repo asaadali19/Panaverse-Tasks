cab84487763c86e5b91f9d54b095415824abe251
asad ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-09 17:39:29 +0500

    Create task31.js
    done

---
+++ task31.js
@@ -0,0 +1,17 @@
+let username = ["admin", "asad", "ali", "saqib","wasif"];
+// let username= [];
+
+for(i=0; i<username.length; i++){
+
+    if(username[i]=== "admin"){
+        console.log("Hello "+ username[i]+ " , would you like to see a status report?")
+    }
+    else if (username[i]!= "admin"){
+        console.log("Hello "+ username[i] + " thank you for logging in again.")
+    }
+    
+    
+}
+if (username.length===0){
+    console.log("We need to find some users!")
+}

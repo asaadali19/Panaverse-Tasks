710c5da8d8b1cbb4653e220653bd8cf7b3a31395
asad ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-09 11:16:26 +0500

    Create task9.js
    task 9 is completed

---
+++ task9.js
@@ -0,0 +1,4 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let Favorite_Number= 5;
+console.log("my favourite number is " + Favorite_Number);

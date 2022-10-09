902a6f46584388b5c11c8fd4243ded5fbd41ce39
asad ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-09 16:59:46 +0500

    Create task27.js
    task 27 is done

---
+++ task27.js
@@ -0,0 +1,17 @@
+
+function points(alien_color){
+    if(alien_color=== "green"){
+        return console.log("player earned 5 points");
+    }
+    else if(alien_color==="yellow")
+    {
+        return "player earned 10 points";
+    }
+    else{
+        return "player earned 15 points";
+    }
+    }
+    points("green");
+    //points("blue");
+    //points("yellow")
+    

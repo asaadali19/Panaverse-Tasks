3de2cbd2e726ef94635fb0b9e97ecf2ddf3cfd22
Muhammad Asad Ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-10 10:04:48 +0500

    Create task35.js
    done

---
+++ task35.js
@@ -0,0 +1,14 @@
+let animal_list = ["cat", "dog", "lion"];
+
+for (i=0; i<animal_list.length; i++){
+    if( animal_list[i]==="cat"){
+        console.log(animal_list[i] + " is a cute animal");
+    }
+    else if( animal_list[i]==="dog"){
+        console.log(animal_list[i] + " saves you from theifes");
+    }
+    else{
+        console.log(animal_list[i]+ " is the king of jungle");
+    }
+}
+console.log("\nthe common things are their legs(4)");

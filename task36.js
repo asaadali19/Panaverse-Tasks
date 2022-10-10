8510c24598216f680eb5c0db5c38bc5103122d07
Muhammad Asad Ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-10 10:08:55 +0500

    Create task36.js
    done

---
+++ task36.js
@@ -0,0 +1,6 @@
+function make_shirt( size, message ){
+    console.log("the size of shirt is " +size+ " and the message that will print on shirt is " +message);
+
+}
+
+make_shirt(5," this is asad");

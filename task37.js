466b30911b70e759cb923a50b2b2e66023f9f6ad
Muhammad Asad Ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-10 10:15:15 +0500

    Create task37.js
    done

---
+++ task37.js
@@ -0,0 +1,9 @@
+function make_shirt( size="large", message="i love javascript" ){
+    console.log("the size of shirt is " +size+ " and the message that will print on shirt is " +message);
+
+}
+
+
+make_shirt("'I love javascript'")
+make_shirt(size = 'medium', message = 'I love javascript');
+make_shirt(size='small', message='JS is in demand');

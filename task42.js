fb757b5a406a5a5e57779b69ab0a636c4a1e7c66
Muhammad Asad Ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-10 11:02:30 +0500

    Create task42.js
    done

---
+++ task42.js
@@ -0,0 +1,15 @@
+let magician_names = ["asad", "ali", "saqib"];
+
+function show_magicians(magician_names){
+for(i=0; i<magician_names.length; i++){
+    console.log(magician_names[i]);
+}
+}
+show_magicians(magician_names);
+
+function make_great(magician_names){
+    for(i=0; i<magician_names.length; i++){
+        console.log("Great "+ magician_names[i]);
+    }
+}
+make_great(magician_names);

a084191c7d3dc6a3d5dfb0c2cea21456c6bebd0a
Muhammad Asad Ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-10 10:19:39 +0500

    Create task38.js
    done

---
+++ task38.js
@@ -0,0 +1,7 @@
+function describe_city(city, country='Canada'){
+   console.log(city+ " is in " + country);
+}
+    
+describe_city('charlotte');
+describe_city('ontarieo');
+describe_city('sagaland', 'india');

ad01e3613b49a545279af7effc0c3ea523765a92
Muhammad Asad Ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-10 09:58:40 +0500

    Create task34.js
    done

---
+++ task34.js
@@ -0,0 +1,8 @@
+let favourtie_pizzas = ["top-loaded", "fajita", "supreme"];
+
+for(i=0; i<favourtie_pizzas.length; i++){
+    
+    console.log("I like "+ favourtie_pizzas[i]+ " pizza");
+}
+
+console.log(" i like pizza very muchi like pizza very i like pizza very muchi like pizza very muchi like pizza very muchi like pizza very muchi like pizza very muchi like pizza very muchi like pizza very muchi like pizza very muchmuchi like pizza very muchi like pizza very muchi like pizza very muchi like pizza very muchi like pizza very muchi like pizza very muchi like pizza very muchi like pizza very muchi like pizza very muchi like pizza very muchi like pizza very muchi like pizza very muchi like pizza very muchi like pizza very much")

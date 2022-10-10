5d689c853372fffebcb7ce520025554a3f14064b
Muhammad Asad Ali <17-cp-74@students.uettaxila.edu.pk>
2022-10-10 12:57:23 +0500

    Create task32.js
    done

---
+++ task32.js
@@ -0,0 +1,18 @@
+let current_users = ['admin', 'saqib', ' saleem', 'asad', 'ali'];
+let new_users = ['admin', 'waqas', ' saleem', 'jind', 'jaan'];
+
+
+for ( let i = 0; i<current_users.length; i++){
+
+    for ( j=0; j<new_users.length;j++){
+
+        if( current_users[i]=== new_users[j]){
+            console.log(current_users[i] +" need to enter a new username");
+        }
+        else{
+            
+        }
+    }
+    
+}
+
